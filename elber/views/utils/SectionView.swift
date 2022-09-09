1f5d3ce9a78568c03e01f6190498bbc23a616739
mnava1418 <martin@mnavapena.com>
2022-09-08 20:08:52 -0400

    Prepare account view

---
+++ elber/views/utils/SectionView.swift
@@ -0,0 +1,16 @@
+//
+//  SectionView.swift
+//  elber
+//
+//  Created by Martin Nava on 08/09/22.
+//
+
+import UIKit
+
+class SectionView: UIView {
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.layer.cornerRadius = 16
+    }
+}
