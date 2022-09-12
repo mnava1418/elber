4ff06da41d71aa3adb3358b8d11f09401442c4d3
mnava1418 <martin@mnavapena.com>
2022-09-12 19:04:36 -0400

    set user data in Account view

--- elber/views/AccountViewController.swift
+++ elber/views/AccountViewController.swift
@@ -10,10 +10,14 @@ import FirebaseAuth
 
 class AccountViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        getUserData()
     }
     
     @IBAction func logOut(_ sender: Any) {
@@ -22,6 +26,19 @@ class AccountViewController: UIViewController {
             performSegue(withIdentifier: "userLogOut", sender: nil)
         } catch let signOutError as NSError {
             AlertsUtil.showNotification(title: "Error", message: signOutError.localizedDescription, viewController: self)
+        }
+    }
+    
+    private func getUserData() {
+        // guard let user = Auth.auth().currentUser
+        guard let user = Auth.auth().currentUser else { return }
+        
+        if let displayName = user.displayName {
+            nameLabel.text = displayName
+        }
+        
+        if let email = user.email {
+            emailLabel.text = email
         }
     }
 }
