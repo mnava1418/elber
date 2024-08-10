590f5f65166a6d05e3d1895d391c1d8dda6fdb52
mnava1418 <martin@mnavapena.com>
2024-08-09 21:36:05 -0400

    ADD: Init project

--- index.js
+++ index.js
@@ -3,11 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-<<<<<<< HEAD
 import App from './src/App';
-=======
-import App from './App';
->>>>>>> c93409499068fcd66437c8ad6b6620a335080666
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
