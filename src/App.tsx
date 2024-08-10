d2998e2f9c328e81cddeee6f36a59612066cadb8
mnava1418 <martin@mnavapena.com>
2024-08-09 21:32:06 -0400

    ADD: Init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>Elber, mi asistente personal</Text>
+    </View>
+  )
+}
+
+export default App
