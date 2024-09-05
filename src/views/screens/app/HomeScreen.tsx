58a2d81fb0f6ca18d2a7c62954172bcb1b2cbfe0
mnava1418 <martin@mnavapena.com>
2024-09-04 20:28:03 -0400

    UPDATE: rename screens

---
+++ src/views/screens/app/HomeScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const HomeScreen = () => {
+    return (
+        <Text>Welcome to Elber!</Text>
+    )
+}
+
+export default HomeScreen
--- src/views/screens/auth/AuthInitScreen.tsx
+++ src/views/screens/auth/AuthInitScreen.tsx
@@ -1,18 +1,22 @@
 import React, { useEffect } from 'react'
 import { Animated, View } from 'react-native'
-import MainView from '../components/ui/MainView'
-import Title from '../components/ui/Title'
-import useAnimateText from '../../hooks/animations/useAnimateText'
-import CustomButton from '../components/ui/CustomButton'
-import { globalStyles } from '../../styles/mainStyles'
-import CustomText from '../components/ui/CustomText'
-import usePulseImage from '../../hooks/animations/usePulseImage'
+import useAnimateText from '../../../hooks/animations/useAnimateText'
+import usePulseImage from '../../../hooks/animations/usePulseImage'
+import { globalStyles } from '../../../styles/mainStyles'
+import CustomButton from '../../components/ui/CustomButton'
+import CustomText from '../../components/ui/CustomText'
+import MainView from '../../components/ui/MainView'
+import Title from '../../components/ui/Title'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { useNavigation, NavigationProp } from '@react-navigation/native'
+import { StackNavigationProps } from '../../Elber'
 
-const logo = require('../../assets/images/dot.png')
+const logo = require('../../../assets/images/dot.png')
 
-const HomeScreen = () => {
+const AuthInitScreen = () => {
     const {animatedText, animateText} = useAnimateText()
     const {scaleImage, pulseImage} = usePulseImage(400, 1.1)
+    const { bottom } = useSafeAreaInsets()
     
     useEffect(() => {
       animateText('Hola, Soy Elber', 50)
@@ -20,6 +24,8 @@ const HomeScreen = () => {
 
       return () => pulseImage.stop()
     }, [scaleImage])
+
+    const navigation = useNavigation<NavigationProp<StackNavigationProps>>()
     
     return (
         <MainView style={{justifyContent: 'space-between', alignItems:'center'}}>
@@ -32,15 +38,20 @@ const HomeScreen = () => {
                 <Title style={{marginTop: 24, fontSize: 40}}>{animatedText}</Title>
                 <CustomText style={{textAlign: 'center', marginHorizontal: 24, marginTop: 16}}>Bienvenido a tu asistente virtual. Estoy listo para ayudarte en todo lo que necesites.</CustomText>
             </View>
-            <View style={{width: '100%', marginBottom: 72}}>
-                <CustomButton type='primary' label='Crea tu cuenta'/>
+            <View style={{width: '100%', paddingBottom: bottom + 24}}>
+                <CustomButton type='primary' label='Crea tu cuenta' onPress={() => {console.log('crear cuenta')}}/>
                 <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'baseline', marginTop: 16}}>
                     <CustomText style={{fontSize: globalStyles.btnText.fontSize}}>¿Ya tienes cuenta?</CustomText>
-                    <CustomButton type='transparent' label='Inicia sesión' style={{ paddingHorizontal: 0, paddingVertical: 0, marginLeft: 5 }}/>
+                    <CustomButton 
+                        type='transparent' 
+                        label='Inicia sesión' 
+                        style={{ paddingHorizontal: 0, paddingVertical: 0, marginLeft: 5 }}
+                        onPress={() => {navigation.navigate('Login')}}
+                    />
                 </View>
             </View>
         </MainView>
     )
 }
 
-export default HomeScreen
+export default AuthInitScreen
