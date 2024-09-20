2ddcab1adcbd6013e6581937b793d588fa84b499
mnava1418 <martin@mnavapena.com>
2024-09-19 22:29:51 -0400

    UPDATE: set user in global state after login

--- src/store/actions/auth.actions.ts
+++ src/store/actions/auth.actions.ts
@@ -1,6 +1,11 @@
-import { AuthAction } from "../reducers/auth.reducer";
+import { AuthAction, AuthUser } from "../reducers/auth.reducer";
 
 export const setIsAuthenticated = (isAuthenticated: boolean): AuthAction => ({
     type: 'SET_AUTHENTICATED',
     payload: isAuthenticated
+})
+
+export const setAuthenticatedUser = (user: AuthUser): AuthAction => ({
+    type: 'SET_AUTHENTICATED_USER',
+    payload: user
 })
--- src/store/reducers/auth.reducer.ts
+++ src/store/reducers/auth.reducer.ts
@@ -1,18 +1,28 @@
 export type AuthState = {
-    isAuthenticated: boolean
+    isAuthenticated: boolean,
+    user: AuthUser
+}
+
+export type AuthUser ={
+    name: string,
+    email: string
 }
 
 export const initialAuthState: AuthState = {
-    isAuthenticated: false
+    isAuthenticated: false,
+    user: { name: '', email: '' }
 }
 
 export type AuthAction = 
 | {type: 'SET_AUTHENTICATED', payload: boolean}
+| {type: 'SET_AUTHENTICATED_USER', payload: AuthUser}
 
 export const authReducer = (state: AuthState, action: AuthAction): AuthState => {
     switch (action.type) {
         case 'SET_AUTHENTICATED':
             return {...state, isAuthenticated: action.payload}
+        case 'SET_AUTHENTICATED_USER':
+            return {...state, user: action.payload}
         default:
             return state
     }
