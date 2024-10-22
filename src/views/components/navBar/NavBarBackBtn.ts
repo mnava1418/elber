53048e80d537b55829a59184d3dd6248a7b19d85
mnava1418 <martin@mnavapena.com>
2024-10-22 19:26:33 -0400

    UPDATE: set any as type

--- src/views/components/navBar/NavBarBackBtn.ts
+++ src/views/components/navBar/NavBarBackBtn.ts
@@ -2,7 +2,7 @@ import { NavigationProp } from "@react-navigation/native"
 import { CustomNavBtnProps } from "../../../interfaces/ui.interface"
 import { StackNavigationProps } from "../../Elber"
 
-const NavBarBackBtn = (navigation: NavigationProp<StackNavigationProps>): CustomNavBtnProps => {
+const NavBarBackBtn = (navigation: NavigationProp<any>): CustomNavBtnProps => {
   return {
     icon: 'chevron-back-outline',
     onPress: () => { navigation.goBack() }
