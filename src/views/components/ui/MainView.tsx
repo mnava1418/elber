8f7aa9991e0c37e95cab397c446b3f73b203720a
mnava1418 <martin@mnavapena.com>
2024-10-29 18:58:17 -0400

    UPDATE: set background color as gradient

--- src/views/components/ui/MainView.tsx
+++ src/views/components/ui/MainView.tsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import { ImageBackground, View } from 'react-native'
 import { CustomViewProps } from '../../../interfaces/ui.interface'
-import { globalColors } from '../../../styles/mainStyles'
+import { LinearGradient } from 'react-native-linear-gradient'
 
 const backgroundImage = require('../../../assets/images/mainBackground.png')
 
 const MainView = ({style, children, showBgImage = false}: CustomViewProps) => {
     const getMainView = () => (
-        <View style={[
-            {flex: 1, paddingHorizontal: 20, backgroundColor: globalColors.background},
-            style
-        ]}>
-            {children}
-        </View>
+        <LinearGradient
+            colors={['#000000', '#012E46']}
+            style={{flex: 1}}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 0.60, y: 0.9 }}
+        >
+            <View style={[
+                {flex: 1, paddingHorizontal: 20},
+                style
+            ]}>
+                {children}
+            </View>
+        </LinearGradient>
     )
 
     const getMainViewWithImg = () => (
