61a3e3625aa72f1940204f30790d4e5f9b28b36d
mnava1418 <martin@mnavapena.com>
2024-11-24 16:03:50 -0500

    UPDATE: set view height

--- src/views/screens/app/chat/ChatActions.tsx
+++ src/views/screens/app/chat/ChatActions.tsx
@@ -15,27 +15,24 @@ const ChatActions = (props: ChatActionsProps) => {
     const {visible, setVisible} = props
     const screenHeight = Dimensions.get('window').height
     const {state} = useContext(GlobalContext)
-    const selectedMessage = selectSelectedMeasure(state.chat)
-    const actionsRef = useRef<View>(null)
+    const selectedMessage = selectSelectedMeasure(state.chat)    
     const [customStyle, setCustomStyle] = useState<StyleProp<ViewStyle>>({})
 
     useEffect(() => {
-        if(visible && selectedMessage && actionsRef.current) {
-            actionsRef.current.measure((fx, fy, width, height) => {
-                const ALERTA_HEIGHT = height + 16
-                const messageLayout = selectedMessage.layout
-                const spaceAbove = messageLayout.py
-                const spaceBelow = screenHeight - (messageLayout.py + messageLayout.height)
-                const showAbove = spaceBelow < ALERTA_HEIGHT && spaceAbove >= ALERTA_HEIGHT
-                let top = showAbove ? messageLayout.py - ALERTA_HEIGHT : messageLayout.py + messageLayout.height
-                top = top + 8
-                
-                if(messageLayout.pv === 'left') {
-                    setCustomStyle({position: 'absolute', top, left: 32})
-                } else {
-                    setCustomStyle({position: 'absolute', top, right: 32})
-                }
-            })
+        if(visible && selectedMessage) {            
+            const ALERTA_HEIGHT = 216
+            const messageLayout = selectedMessage.layout
+            const spaceAbove = messageLayout.py
+            const spaceBelow = screenHeight - (messageLayout.py + messageLayout.height)
+            const showAbove = spaceBelow < ALERTA_HEIGHT && spaceAbove >= ALERTA_HEIGHT
+            let top = showAbove ? messageLayout.py - ALERTA_HEIGHT : messageLayout.py + messageLayout.height
+            top = top + 8
+            
+            if(messageLayout.pv === 'left') {
+                setCustomStyle({position: 'absolute', top, left: 32})
+            } else {
+                setCustomStyle({position: 'absolute', top, right: 32})
+            }            
         }
     }, [selectedMessage])
     
@@ -45,7 +42,7 @@ const ChatActions = (props: ChatActionsProps) => {
                 <Modal transparent={true} visible={visible} animationType="fade">
                     <Pressable style={{flex: 1}} onPress={() => {setVisible(false)}}>
                         <View style={globalStyles.modalBackground}>
-                            <View ref={actionsRef} style={[globalStyles.alertContainer, customStyle, {width: 250}]}>
+                            <View style={[globalStyles.alertContainer, customStyle, {width: 250, height: 200}]}>
                                 {chatActions.map((action, index) => (
                                     <ChatActionItem key={index} message={selectedMessage.content} action={action} isLast={index === chatActions.length - 1} setVisible={setVisible} />
                                 ))}
