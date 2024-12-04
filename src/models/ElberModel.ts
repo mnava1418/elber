572b74022af06646672e7548494566c3a846d10d
mnava1418 <martin@mnavapena.com>
2024-12-03 19:24:04 -0500

    ADD: create elber model to handle speech

---
+++ src/models/ElberModel.ts
@@ -0,0 +1,41 @@
+import Tts, { Voice } from "react-native-tts";
+import { ElberVoice } from "../store/reducers/elber.reducer";
+
+class ElberModel {
+
+    private static instance: ElberModel
+
+    constructor() {        
+    }
+
+    public static getInstance(): ElberModel {
+        if(!ElberModel.instance) {
+            ElberModel.instance = new ElberModel()
+        }
+
+        return ElberModel.instance
+    }
+
+    public setVoice(voice: ElberVoice) {
+        if(voice.id.trim() !== '' && voice.language.trim() !== '') {
+            Tts.setDefaultLanguage(voice.language)
+            Tts.setDefaultVoice(voice.id)
+        }        
+    }
+
+    public async getAvailableVoices(): Promise<Voice[]> {
+        const availableVoices: Voice[] = await Tts.voices()
+        .catch(() => [])
+
+        const spanishVoices = availableVoices.filter(voice => voice.language.trim().startsWith('es'))
+
+        if(spanishVoices.length > 0) {
+            return spanishVoices
+        } else {
+            const englishVoices = availableVoices.filter(voice => voice.language.trim() === 'en-US')       
+            return englishVoices
+        }
+    }
+}
+
+export default ElberModel
